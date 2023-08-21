e4e22564c6ad06eb2b924e5cae418f06033ad1cd
Emon Ahmed <imonshomon@gmail.com>
2023-08-21 10:42:44 +0600

    Initial commit

---
+++ src/components/fristcom.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>Home Compoent</h1>
+    <h1>name:{{ getName() }}</h1>
+    <h1>name:{{ email }}</h1>
+
+    <button  v-on:click="test()" >Click</button>
+    <button @click="count++">Add 1</button>
+<p>Count is: {{ count }}</p>
+</template>
+
+
+
+<script>
+  export default{
+    name:'fristcom',
+    
+ data(){
+
+      return {
+        
+        count:0           
+      
+      }
+ },
+      methods:{
+        getName(){
+             return this.count+1
+        },
+
+        test(){
+            alert('fuctin called')
+
+            
+                   
+             
+         
+      }
+    }
+  }
+</script>
---
+++ src/components/twowaydatabinding.vue
@@ -0,0 +1,41 @@
+<template>
+<!-- 
+<input type="text" v-model="count" >
+<h1>{{count}}</h1> -->
+
+
+<input type="text" v-model="email" >
+<br/>
+<input type="text" v-model="password">
+
+<button @click="getValues()">get values</button>
+</template>
+
+
+
+
+<script>
+export default{
+    name:'twowaydatabinding.vue',
+
+
+data(){
+
+return{
+   email:null,
+   password:null,
+
+}
+},
+
+methods:{
+
+    getValues(){
+          console.log('ki khobor bondo',this.email,this.password)
+          
+    }
+}
+
+}
+
+</script>
